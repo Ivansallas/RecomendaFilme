61f0494c70c44b9a3cf845187faf58f4560737d4
Ivansallas <kiss_ivan@hotmail.com>
2025-06-17 23:33:24 -0300

    30

--- sketch.js
+++ sketch.js
@@ -1,53 +1,66 @@
 let campoIdade;
 let campoFantasia;
 let campoAventura;
+let imagemAtual;
+let imagens = {};
 
-function setup() {
+async function setup() {
   createCanvas(800, 400);
   createElement("h2", "Recomendador de filmes");
   createSpan("Sua idade:");
   campoIdade = createInput("5");
   campoFantasia = createCheckbox("Gosta de fantasia?");
   campoAventura = createCheckbox("Gosta de aventura?");
+
+  //adicionar as imagens conforme abaixo
+  //buscar as imagens na web e baixar para o mesmo diretório do sketch
+  imagens.chihiro = await loadImage("chihiro.jpeg");
+  imagens.pi = await loadImage("pi.jpeg");
 }
 
 function draw() {
   background("white");
-  let idade = campoIdade.value();
+  if (!imagens || Object.keys(imagens).length === 0) {
+    fill("black");
+    textAlign(CENTER, CENTER);
+    textSize(24);
+    text("Carregando imagens...", width / 2, height / 2);
+    return;
+  }
+
+  let idade = int(campoIdade.value());
   let gostaDeFantasia = campoFantasia.checked();
   let gostaDeAventura = campoAventura.checked();
-  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
 
-  fill(color(76, 0, 115));
-  textAlign(CENTER, CENTER);
-  textSize(38);
-  text(recomendacao, width / 2, height / 2);
+  let chaveImagem = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+  imagemAtual = imagens[chaveImagem];
+
+  if (imagemAtual) {
+    imageMode(CENTER);
+    image(imagemAtual, width / 2, height / 2, 400, 300);
+  }
 }
 
 function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
-  if (idade >= 10) {
-    if (idade >= 14) {
-      return "O menino que descobriu o vento";
+  if (idade >= 14) {
+    return "vento";
+  } else if (idade >= 12) {
+    if (gostaDeFantasia || gostaDeAventura) {
+      return "aranhaverso";
     } else {
-      if (idade >= 12) {
-        if (gostaDeFantasia || gostaDeAventura) {
-          return "Homem aranha: no aranhaverso";
-        } else {
-          return "Ladrões de bicicleta";
-        }
-      } else {
-        if (gostaDeFantasia) {
-          return "As aventuras de pi";
-        } else {
-          return "Depois da chuva";
-        }
-      }
+      return "ladroes";
+    }
+  } else if (idade >= 10) {
+    if (gostaDeFantasia) {
+      return "pi";
+    } else {
+      return "chuva";
     }
   } else {
     if (gostaDeFantasia) {
-      return "A viagem de chihiro";
+      return "chihiro";
     } else {
-      return "O feitiço do tempo";
+      return "tempo";
     }
   }
 }
